ea4196be247f47d388943cda65a62a8605f09265
iremsu.menekse <iremsu.menekse77@gmail.com>
2019-01-27 13:25:16 +0300

    String

---
+++ Ders2/src/Main.java
@@ -0,0 +1,54 @@
+public class Main {
+    public static void main(String[] args){
+        int a=5;
+        System.out.println("bu bir integer değer"+a);
+        Integer b=5;
+        System.out.println("bu bir integer değer"+b);
+        Integer c=new Integer(5);
+        System.out.println("bu bir integer değer"+c);
+        //long b =300000000000L
+        int irem;
+        irem=97;
+        irem=7;
+        System.out.println(irem);
+        //en son atadığımız değeri bastırır
+        long along=1L;
+        System.out.println("bu bir long değeridir:"+along);
+        Long bLong=2L;
+        System.out.println("Bu bir long değer:"+bLong);
+        Long cLong=new Long(2);
+        System.out.println("bu bir long değer;"+cLong);
+        short aShort=50;
+        System.out.println("bu bir short değer"+aShort);
+        Short bShort=50;
+        System.out.println("bu bir short değerdir"+bShort);
+        Short cShort= new Short((short)50);
+        System.out.println("bu bir short değer"+cShort);
+        byte aByte=20;
+        System.out.println("bu bir byte değer"+aByte);
+        Byte bByte=20;
+        System.out.println("bu bir byte değer"+bByte);
+        Byte cByte=new Byte((byte)20);
+        System.out.println("bu bir byte değerdir"+cByte);
+        char achar='a';
+        System.out.println("bu bir char değerdir"+achar);
+        Character bChar='a';
+        System.out.println("bu bir char değerdir"+bChar);
+        Character cChar=new Character('a');
+        System.out.println("bu bir char değerdir"+cChar);
+
+        String aString="abode";
+        System.out.println("bu bir string değer"+aString);
+        String bString=new String("abode");
+        System.out.println("bu bir string değerdir"+bString);
+
+
+
+
+
+
+
+
+
+    }
+}
